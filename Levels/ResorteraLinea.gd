322a7e2b281b60d5885afd2bd4d31a4989fcdceb
Denzypaulito <124909699+Denzypaulito@users.noreply.github.com>
2023-10-26 18:49:50 -0600

    Correccion de error menor Señal

--- Levels/ResorteraLinea.gd
+++ Levels/ResorteraLinea.gd
@@ -8,7 +8,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var cbird = resortera.last_bird
 	if cbird:
 		visible = true
